2342d0b5458776ceaa781b46e6babbc67f92e78d
richmond-gakpetor <gakpetorr01@gmail.com>
2022-06-16 15:32:00 +0000

    Refactor Textstyles

---
+++ my_contact_app/lib/constants/styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const kAppBarTitleStyle = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+  color: Colors.black,
+);
+
+const kContactTextStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+);
